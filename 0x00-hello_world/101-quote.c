9b60934a9d2451b64b015d2adae8f6d42e718275
Pealzhub <chillpeal@yahoo.com>
2023-02-20 15:39:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdio.h>
+
 /**
  * main - Entry
+ *
  * Return: 1 (success)
  */
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n”, stdout);
+fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n”, 59);
 return (1);
 }
-
