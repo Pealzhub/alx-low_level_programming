12bc95e7b7a939010e27a472f7bece9a2766a54e
Pealzhub <chillpeal@yahoo.com>
2023-03-20 12:55:38 -0700

    updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,10 @@
 /**
 * main - prints the name of the file
 *
-* Return: Always 0
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n", ‘FILE’);
 return (0);
 }
